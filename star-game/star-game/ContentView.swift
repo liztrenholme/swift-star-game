91d1d3af0bdcabd9494ec84b9c78001b326675e5
liztrenholme <liztrenholme@gmail.com>
2024-12-14 03:39:39 -0500

    initial buildout for main screen

--- star-game/star-game/ContentView.swift
+++ star-game/star-game/ContentView.swift
@@ -8,14 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var counter = 0
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Text("\(counter)")
+//          Button("Increase Count"){ counter += 1 }
+//          Button(action: { counter = counter - 1 }) {
+//            Image(systemName: "arrow.down")
+//          }
+//          .buttonStyle(.plain)
         }
-        .padding()
+        ZStack {
+            Color(.systemBrown)
+                .ignoresSafeArea()
+            VStack {
+                Text("Star Collector")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+                Image("martianchroniclesmobile")
+                    .resizable()
+                    .cornerRadius(10)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.all)
+                HStack {
+                    Button {
+                        counter += 1
+                    } label: {
+                        Image("blackstar")
+                            .resizable()
+                            .cornerRadius(10)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.all)
+
+                    }
+                    Button {
+                        counter += 3
+                    } label: {
+                        Image("redstar")
+                            .resizable()
+                            .cornerRadius(10)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.all)
+                    }
+                    Button {
+                        counter += 9
+                    } label: {
+                        Image("greenstar")
+                            .resizable()
+                            .cornerRadius(10)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.all)
+                    }
+                    Button {
+                        counter += 2
+                    } label: {
+                        Image("yellowstar")
+                            .resizable()
+                            .cornerRadius(10)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.all)
+                    }
+                }
+            }
+        }
     }
 }
 
